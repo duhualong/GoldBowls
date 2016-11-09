dd2d1d841206ec7f265a29be902ebbea6035fb52
duhualong <1456198324@qq.com>
2016-11-09 16:49:41 +0800

    注册的部分页面3

--- app/src/main/java/haowei/computer/goldbowl/MainActivity.java
+++ app/src/main/java/haowei/computer/goldbowl/MainActivity.java
@@ -2,6 +2,8 @@ package haowei.computer.goldbowl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //设置不同字体不同颜色
+        TextView tv= (TextView) findViewById(R.id.textView);
+        String str="测试字体<font color='#FF0000'><font>颜色</font></font>";
+        tv.setText(Html.fromHtml(str));
     }
 }
