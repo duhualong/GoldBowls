2942dd6c6a7360231c49158bb19e6b332a7be621
duhualong <1456198324@qq.com>
2016-11-10 17:50:35 +0800

    忘记密码

--- app/src/main/java/haowei/computer/goldbowl/MainActivity.java
+++ app/src/main/java/haowei/computer/goldbowl/MainActivity.java
@@ -10,10 +10,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //设置不同字体不同颜色
-        TextView tv= (TextView) findViewById(R.id.textView);
-        String str="测试字体<font color='#FF0000'><font>颜色</font></font>";
-        tv.setText(Html.fromHtml(str));
+        setContentView(R.layout.fragment_register_second);
+//        //设置不同字体不同颜色
+//        TextView tv= (TextView) findViewById(R.id.textView);
+//        String str="测试字体<font color='#FF0000'><font>颜色</font></font>";
+//        tv.setText(Html.fromHtml(str));
     }
 }
